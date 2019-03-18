f2443de1d8c20d9f7995eba2c21819a3ab33d389
Pradeep Kumar <pradeepisro49@gmail.com>
2019-03-17 23:06:21 -0700

    Updated file, still not solved

--- llvmgsocexercise.cxx
+++ llvmgsocexercise.cxx
@@ -1,17 +1,17 @@
-typedef struct __attribute__((aligned(8))) {
+typedef struct{
     int (**a)(...);
     int b;
-    char c[4];
-} S;
+} __attribute__((packed)) S;
 
 S *s = 0;
 
 void temp(S *s, int a) {
     s = new S;
-    s->b = 42;
     float arr[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f};
     for (int i = 0; i < 8; i++) {
         arr[i] = arr[i] * arr[i] / __builtin_sqrt(arr[i] + a);
     }
+    s->b = arr[a % 8];
+    s->b = s->b - 42;
     return;
 }
